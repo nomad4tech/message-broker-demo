074feb41bd9c288f3b8aae7cc8969757b27a4987
Alexander <alex.sav4387@gmail.com>
2024-09-08 23:23:52 +0300

    add common libs module

---
+++ common/src/main/java/tech/nomad4/model/PostMsgDto.java
@@ -0,0 +1,14 @@
+package tech.nomad4.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PostMsgDto {
+
+    private String name;
+
+    private String description;
+
+}
